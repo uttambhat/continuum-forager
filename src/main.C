1af9745b28b12a5a7683084d8c3cc543ebe73416
uttambhat <uttambhat13@gmail.com>
2019-07-17 11:52:14 -0700

    Added preprocessor switch for OpenGL visualization

--- src/main.C
+++ src/main.C
@@ -4,33 +4,42 @@
 #include "../include/resource_map.h"
 #include "../include/generic/output_functions.h"
 #include "../include/foraging_output_functions.h"
+
+#define VISUALIZE 0
+
+#if VISUALIZE
 #include "GL/freeglut.h"
 #include "GL/gl.h"
+#endif
+
 #include <thread>
 #include <chrono>
+
 using namespace std;
 
 void run_simulation()
 {
-	// Declare resource_map and set regrowth rate. Args: number of points on the map, Brownian step-size (default set to 0.01)
+	// Declare resource_map. Args: (number of points on the map, Brownian step-size (default set to 0.01))
 	resource_map_Brownian MapA(100000,0.01L);
-	double regrowth_rate=10000.L;
+	
+	// Set regrowth rate of map
+	double regrowth_rate=1000.L;
 	cout << MapA.measure_zeta(1000) << endl;
 	cout << MapA.total_resources() << endl;
 
-	// set value of zeta
+	// Uncomment to set value of zeta
 	//MapA.set_zeta(1.L);
 	
 	// set mesh density (does not discretize the actual process. The mesh is only for increasing simulation speed and for visualization)
 	MapA.map_to_mesh(100);
 
 	// resolution of delta-time for simulation
-	const double dtime=0.01L;
+	const double dtime=0.1L;
 	
 	double time=0.L;
 
 	// Initialize Population vector
-	int number_of_species=2;
+	int number_of_species=3;
 	vector<forager_population> Forager_Population;
 	Forager_Population.resize(number_of_species);
 	
@@ -47,13 +56,15 @@ void run_simulation()
 	}
 
 	// set output file for population summary
-	ofstream output_population_size; output_population_size.open("data/population_size_1");
+	ofstream output_population_size; output_population_size.open("data/population_size_2");
 
+#if VISUALIZE
 	// Background color of visualization
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 #if D==3
 	glRotatef(30.,0.0,1.0,.0);
 	glRotatef(30.,1.0,.0,0.0);
+#endif
 #endif
 
 	// Loop around for maximum number of iterations
@@ -79,15 +90,17 @@ void run_simulation()
 		for(int n=0; n<Forager_Population.size(); n++) all_extinct=all_extinct&&(Forager_Population[n]._member.size()==0);
 		if(all_extinct) break;
 
-		// Uncomment line below to slow the simulation down
-		//this_thread::sleep_for(chrono::milliseconds(0));
-
 		// Regrow resource
 		MapA.regrow((int)(regrowth_rate*dtime));
 		
 		// update time
 		time += dtime;
 
+		// Uncomment line below to slow the simulation down without changing dtime (debug tool). However, for changing simulation time resolution, use dtime
+		//this_thread::sleep_for(chrono::milliseconds(0));
+
+#if VISUALIZE
+		// The next segment till the end of the run_simulation() function is only for the visualization. Ignore if visualization is switched off.
 		// update visualization. Use the if condition to adjust time-resolution of visualization
 		if(i%1==0)
 		{
@@ -116,9 +129,9 @@ void run_simulation()
 			{
 				glPointSize((((float)n+1.))*3.);
 				glBegin(GL_POINTS);
+				glColor4f((0.2+0.8*(float)(number_of_species-n)/(float)number_of_species),0.,0.,0.2);
 				for(int i=0; i<Forager_Population[n]._member.size(); i++)
 				{
-					glColor4f((0.2+0.8*(float)(number_of_species-n)/(float)number_of_species),0.,0.,0.2);
 #if D==2
 					glVertex2f(2.L*Forager_Population[n]._member[i]._position[0]-1.L,2.L*Forager_Population[n]._member[i]._position[1]-1.L);
 #elif D==3
@@ -127,35 +140,9 @@ void run_simulation()
 				}
 				glEnd();
 			}
-#if D==2
-			glLineWidth(10.0f);
-			glBegin(GL_LINES);
-			glVertex2f(-1.,-1.);
-			glVertex2f(1.,-1.);
-			glEnd();
-			glBegin(GL_LINES);
-			glVertex2f(-1.,-1.);
-			glVertex2f(-1.,1.);
-			glEnd();
-#elif D==3
-			glLineWidth(10.0f);
-			glBegin(GL_LINES);
-			glVertex3f(-1.,-1.,-1.);
-			glVertex3f(1.,-1.,-1.);
-			glEnd();
-			glBegin(GL_LINES);
-			glVertex3f(-1.,-1.,-1.);
-			glVertex3f(-1.,1.,-1.);
-			glEnd();
-			glBegin(GL_LINES);
-			glVertex3f(-1.,-1.,-1.);
-			glVertex3f(-1.,-1.,1.);
-			glEnd();
-#endif
-
-
 			glFlush();
 		}
+#endif
 	}
 	output_population_size.close();
 
@@ -164,68 +151,19 @@ void run_simulation()
 
 int main(int argc, char **argv)
 {
-	if(argc==1)
-	{
-		glutInit(&argc, argv);
-		glutInitDisplayMode(GLUT_SINGLE|GLUT_RGBA);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		glEnable( GL_BLEND );
-   		glutInitWindowSize(1000, 1000);
-		glutInitWindowPosition(100, 100);
-		glutCreateWindow("Foraging simulation");
-		glutDisplayFunc(run_simulation);
-		glutMainLoop();
-	}
-	else if(argc>1) run_simulation();
+#if VISUALIZE
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGBA);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable( GL_BLEND );
+	glutInitWindowSize(1000, 1000);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Foraging simulation");
+	glutDisplayFunc(run_simulation);
+	glutMainLoop();
+#else
+	run_simulation();
+#endif
 	return 0;
 }
-
-
-
-
-/*	ofstream output_forager_locations; char outputfile_forager_locations[]="data/forager_locations/t=00000";
-	ofstream output_map_files; char outputfile_map_files[]="data/map_files/t=00000";
-	
-	for(int i=0; i<10; i++)
-	{
-		update_filename_index(outputfile_forager_locations,i,5); 
-		update_filename_index(outputfile_map_files,i,5);
-
-		print_forager_populations_to_file(Forager_Population, outputfile_forager_locations);
-		MapA.print_mesh_to_file(outputfile_map_files);
-	}
-
-	
-*/
-
-
-/*		if(time>500.L)
-		{
-			for(int n=0; n<Forager_Population.size(); n++)
-			{
-				sumpop[n] += (double)Forager_Population[n]._member.size();
-				sumpop2[n] += pow((double)Forager_Population[n]._member.size(),2);
-			}
-			sumres += MapA._resource_count;
-			sumres2 += pow(MapA._resource_count,2); 
-			N+=1.L;
-		}
-		
-
-
-	}
-	for(int n=0; n<number_of_species; n++)
-	{
-		avgpop[n] = sumpop[n]/N;
-		avgpop2[n] = sumpop2[n]/N;
-	}
-	avgres = sumres/N;
-	avgres2 = sumres2/N;
-
-	cout << endl;
-	for(int n=0; n<number_of_species; n++) cout << avgpop[n] << '\t' << sqrt(avgpop2[n] - avgpop[n]*avgpop[n]) << '\t'; 
-	cout << avgres << '\t' << sqrt(avgres2-avgres*avgres) << endl;
-*/	
-
-
 
